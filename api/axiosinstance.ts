12e71e7f3ad7f126b8c7014c47827c2fbe29d1ea
Baek <qorgkfbs123@naver.com>
2025-09-28 21:27:34 +0900

    세부 목표 일정 전체 분리

--- api/axiosinstance.ts
+++ api/axiosinstance.ts
@@ -11,21 +11,6 @@ const axiosInstance = axios.create({
 });
 
 
-axiosInstance.interceptors.request.use(
-  (config) => {
-    // 👇 임의의 토큰 'test-token'을 직접 할당합니다.
-    const token = 'test-token';
 
-    // 토큰이 존재하므로 항상 Authorization 헤더를 추가합니다.
-    config.headers['Authorization'] = `Bearer ${token}`;
-
-    // 설정된 config를 반환합니다.
-    return config;
-  },
-  (error) => {
-    // 요청 에러 처리
-    return Promise.reject(error);
-  }
-);
 
 export default axiosInstance;
