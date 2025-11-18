c43b31e67dbcc2da5890627a1fdcea9770a10fa6
gumguard <gawonlee0712@gmail.com>
2025-11-18 19:16:30 +0900

    [fix] 변수명 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { GoogleAnalytics } from '@next/third-parties/google';
 
-const myGaId = process.env.NEXT_PUBLIC_GA_ID || '';
+const myGaId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS || '';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
