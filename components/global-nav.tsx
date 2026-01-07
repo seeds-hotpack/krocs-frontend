1ce93819d149beb2c32f51dc716e2e130ec5116b
gumguard <gawonlee0712@gmail.com>
2026-01-07 22:16:49 +0900

    [fix] 로그아웃 후 기존 날짜 로컬스토리지에서 삭제

--- components/global-nav.tsx
+++ components/global-nav.tsx
@@ -29,6 +29,9 @@ export function GlobalNav() {
     setIsLoggingOut(true)
     try {
       await logout()
+      if (typeof window !== "undefined") {
+        window.localStorage.removeItem("goalPageSelectedDate")
+      }
       markUnauthenticated()
       router.push("/login")
     } catch (error) {
